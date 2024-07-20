7196a47bb66b900c40c501ca730d4bf23fcf2bd2
BaoHuynh2406 <huynhbaomts2004@gmail.com>
2024-07-20 11:14:36 +0700

    fix login page

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <template>
-  <router-view/>
+  <router-view />
 </template>
 
 <style>
+html,
+body,
+#app {
+  height: 100%;
+}
 
+* {
+  font-family: "Poppins", sans-serif;
+  font-weight: 500;
+  font-style: normal;
+}
 </style>
--- src/components/page/login/LoginView.vue
+++ src/components/page/login/LoginView.vue
@@ -1,31 +1,37 @@
 <template>
-    <div class="container-fluid login-bg">
+    <div class="container-fluid login-bg h-100">
         <img src="@/assets/images/hinh_Chua.svg" class="img-fluid position-absolute hinhChua" alt="">
         <img src="@/assets/images/logo_gxTanHung.svg" class="img-fluid position-absolute hinhLogo" alt="">
-        <img src="@/assets/images/chu_gxTanHung.svg" class="img-fluid position-absolute hinhChu" alt="">
-        <div class="row w-100 m-0">
-            <div class="d-flex align-items-center ">
+        <img src="@/assets/images/chu_gxTanHung.svg" class="d-none d-md-inline img-fluid position-absolute hinhChu"
+            alt="">
+        <div class="row w-100 h-100 m-0">
+            <div class="d-flex align-items-center justify-content-center">
                 <div class="col-lg-4 col-md-6 col-sm-8 offset-lg-6 offset-md-6 offset-sm-0">
                     <div class="card">
-                        <div class="card-body p-4">
-                            <h3 class="card-title text-center mb-4 fw-bold">ĐĂNG NHẬP</h3>
+                        <div class="card-body my-3 p-4">
+                            <h1 class="card-title text-center my-4 fw-bold" style="color: #333;">ĐĂNG NHẬP</h1>
                             <div class="form-group">
                                 <label class="fw-bold">Tên đăng nhập: <span class="text-danger">*</span></label>
                                 <input name="username" type="text" class="form-control">
                             </div>
-                            <div class="form-group">
+                            <div class="form-group mt-3">
                                 <label class="fw-bold">Mật khẩu: <span class="text-danger">*</span></label>
                                 <input name="password" type="password" class="form-control">
                             </div>
-                            <div class="form-group d-flex align-items-center justify-content-between">
+                            <div class="form-group mt-3 d-flex align-items-center justify-content-between">
                                 <div class="form-check">
                                     <input type="checkbox" class="form-check-input" id="rememberMe">
                                     <label class="form-check-label" for="rememberMe">Ghi nhớ đăng nhập</label>
                                 </div>
                                 <a href="#" class="forgot-pass">Quên mật khẩu</a>
                             </div>
-                            <div class="text-center">
-                                <button class="btn btn-primary">Đăng nhập</button>
+                            <div class="text-center mt-4">
+                                <button 
+                                    class="btn btn-primary w-100 fw-bold" 
+                                    style="background-color: #409fda; 
+                                    border: none;">
+                                    Đăng nhập
+                                </button>
                             </div>
                         </div>
                     </div>
@@ -46,7 +52,10 @@
 }
 
 .card {
-    background-color: rgba(255, 255, 255, 0.2)
+    background-color: rgba(255, 255, 255, 0.8);
+    border: none;
+    border-radius: 20px;
+    box-shadow: rgba(0, 0, 0, 0.16) 0px 10px 36px 0px, rgba(0, 0, 0, 0.06) 0px 0px 0px 1px;
 }
 
 .hinhChua {
